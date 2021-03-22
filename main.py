f918b19726e52c4f3c6da47788f25802f4b9b8b6
CooperMonsrud <817455@mystma.org>
2021-03-22 15:27:07 +0000

    Mad libs

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-adjectiveOne = "light"
-adverbOne = "quickly"
-verbOne = "growing"
-timeOfNight = "8 PM"
-nameOne = "John"
-typeOfBuilding = "Very Tall"
-numberOne = "13"
-place = "New York City"
-numberTwo = "4"
-adverbTwo = "Hastily"
-nounOne = "Jacky"
-nounTwo = "Cheese"
+adjectiveOne = input("Input an ajective\n")
+adverbOne = input("Input an adverb\n")
+verbOne = input("Input a verb\n")
+timeOfNight = input("Input a time during the night\n")
+nameOne = input("Input a name\n")
+typeOfBuilding = input("Input a building\n")
+numberOne = input("Input a number\n")
+place = input("Input a place\n")
+numberTwo = input("Input a Number\n")
+adverbTwo = input("Input an adverb\n")
+nounOne = input("Input a noun\n")
+nounTwo = input("Input a noun\n")
 print("In the early hours of Wednesday, February 10th, 1880 the town was jolted awake by a " + adjectiveOne + " sound. The sound would turn out to be that of the safe at the First National Bank being " + adverbOne + verbOne + "! At the time, the bank was housed in what we know as the National Hall. At " + timeOfNight + ", the downtown night watchman, " + nameOne + 
 ", was attacked by a group of men and taken to wait behind a " + typeOfBuilding + "near the bank. After what he thought to be about " + numberOne + "hours, he was moved to a " + place + "inside the bank. After" + numberTwo + "hours work, the men decided to " + adverbTwo + "the vault door. Sadly they used far too much " + nounTwo + "! The explosion caused them to panic and grab as much " + nounTwo + 
 "as they could then make a run for the train")

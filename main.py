33c41a9ce5317923591d0b2f3430deae9c24126f
CooperMonsrud <817455@mystma.org>
2021-03-22 15:04:04 +0000

    Madlibs

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-adjectiveOne = light
-adverbOne = quickly
-verbOne = growing
-timeOfNight
+adjectiveOne = "light"
+adverbOne = "quickly"
+verbOne = "growing"
+timeOfNight = "8 PM"
+nameOne = "John"
+typeOfBuilding = "Very Tall"
+numberOne = "13"
+place = "New York City"
+numberTwo = "4"
+adverbTwo = "Hastily"
+nounOne = "Jacky"
+nounTwo = "Cheese"
+print("In the early hours of Wednesday, February 10th, 1880 the town was jolted awake by a " + adjectiveOne + " sound. The sound would turn out to be that of the safe at the First National Bank being " + adverbOne + verbOne + "! At the time, the bank was housed in what we know as the National Hall. At " + timeOfNight + ", the downtown night watchman, " + nameOne + 
+", was attacked by a group of men and taken to wait behind a " + typeOfBuilding + "near the bank. After what he thought to be about " + numberOne + "hours, he was moved to a " + place + "inside the bank. After" + numberTwo + "hours work, the men decided to " + adverbTwo + "the vault door. Sadly they used far too much " + nounTwo + "! The explosion caused them to panic and grab as much " + nounTwo + 
+"as they could then make a run for the train")
